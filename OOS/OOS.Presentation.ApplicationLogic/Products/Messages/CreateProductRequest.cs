a27c9a04dd44dbca45a2ab389e2ee49f0fa64f9c
ORIENTSOFTWARE\nhloc <nguyenhuuloc304@gmail.com>
2018-03-14 13:46:05 +0700

    update code

--- OOS/OOS.Presentation.ApplicationLogic/Products/Messages/CreateProductRequest.cs
+++ OOS/OOS.Presentation.ApplicationLogic/Products/Messages/CreateProductRequest.cs
@@ -1,12 +1,14 @@
 ﻿using OOS.Presentation.ApplicationLogic.Common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace OOS.Presentation.ApplicationLogic.Products.Messages
 {
     public class CreateProductRequest : RequestBase
     {
+        [Required]
         public string Name { get; set; }
     }
 }
