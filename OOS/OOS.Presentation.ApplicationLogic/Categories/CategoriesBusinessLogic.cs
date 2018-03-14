0baa8a05bf1f5f19d35ecc6817373f0e71a04f23
DESKTOP-LS7M6TG\Kiet <kiettruong312@gmail.com>
2018-03-14 15:37:17 +0700

    create business logic layer

---
+++ OOS/OOS.Presentation.ApplicationLogic/Categories/CategoriesBusinessLogic.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using OOS.Domain.Categories.Models;
+using OOS.Infrastructure.Mongodb;
+
+namespace OOS.Presentation.ApplicationLogic.Categories
+{
+    public class CategoriesBusinessLogic : ICategoriesBusinessLogic
+    {
+        private readonly IMapper _mapper;
+        private readonly IMongoDbRepository _mongoDbRepository;
+
+        public CategoriesBusinessLogic(IMapper mapper, IMongoDbRepository mongoDbRepository)
+        {
+            _mapper = mapper;
+            _mongoDbRepository = mongoDbRepository;
+        }
+
+        public void DeleteCategory(string id)
+        {
+            var category = _mongoDbRepository.Get<Category>(id);
+            _mongoDbRepository.Delete(category);
+        }
+    }
+}
---
+++ OOS/OOS.Presentation.WebAPIs/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OOS.Presentation.ApplicationLogic.Categories;
+
+namespace OOS.Presentation.WebAPIs.Controllers
+{
+    [Route("api/Category")]
+    public class CategoryController : Controller
+    {
+
+        private readonly ICategoriesBusinessLogic _categoriesBusinessLogic;
+
+        public CategoryController(ICategoriesBusinessLogic categoriesBusinessLogic)
+        {
+            _categoriesBusinessLogic = categoriesBusinessLogic;
+        }
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            _categoriesBusinessLogic.DeleteCategory(id);
+            return Ok();
+        }
+    }
+}
