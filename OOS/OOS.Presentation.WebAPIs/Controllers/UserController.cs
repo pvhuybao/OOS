b48a1077027b72ffa7031b32fa5a51e119dc21e6
DESKTOP-LS7M6TG\Kiet <kiettruong312@gmail.com>
2018-03-14 16:17:09 +0700

    update code

---
+++ OOS/OOS.Presentation.WebAPIs/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OOS.Presentation.WebAPIs.Controllers
+{
+    [Route("api/User")]
+    public class UserController : Controller
+    {
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public IActionResult Put(string id, [FromBody]CreateUserRequest request)
+        //{
+        //    _usersBusinessLogic.EditUser(request, id);
+        //    return Ok();
+        //}
+    }
+}
