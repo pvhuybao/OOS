7a402841d238bfd4a7f396cbbe8d2293ddb55842
DESKTOP-E8OJBH0\NguyenHuan <huancv1996@gmail.com>
2018-03-22 13:20:40 +0700

    update model Product

--- OOS/OOS.Domain/Products/Models/Product.cs
+++ OOS/OOS.Domain/Products/Models/Product.cs
@@ -20,17 +20,15 @@ namespace OOS.Domain.Products.Models
         [Required]
         public string Name { get; set; }
 
-        [Required]
-        public int Price { get; set; }
-
         public string Description { get; set; }
-
-        [Required]
-        public string Image { get; set; }
 
         [Required]
         public string IdCategory { get; set; }
 
         public ProductStatus? Status { get; set; }
+        
+        public string Details { get; set; }
+
+        public List<ProductTail> ProductTails { get; set; }
     }
 }
---
+++ OOS/OOS.Domain/Products/Models/ProductTail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOS.Domain.Products.Models
+{
+    public class ProductTail
+    {
+        public string Size { get; set; }
+
+        public string Color { get; set; }
+        
+        public int Quantity { get; set; }
+
+        public string Image { get; set; }
+
+        public int Price { get; set; }
+    }
+}
