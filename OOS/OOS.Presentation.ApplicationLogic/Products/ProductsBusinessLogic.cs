5af2aa2d037307ee34f1820d991ec74f99b73937
Ta Xuan Tung <tung.tx1993@gmail.com>
2018-03-29 09:42:54 +0700

    update widget product

--- OOS/OOS.Presentation.ApplicationLogic/Products/ProductsBusinessLogic.cs
+++ OOS/OOS.Presentation.ApplicationLogic/Products/ProductsBusinessLogic.cs
@@ -83,7 +83,7 @@ namespace OOS.Presentation.ApplicationLogic.Products
             {
                 //TimeSpan timeSpan = TimeSpan.FromDays(10);
                 var filter = Builders<Product>.Filter.Empty;
-                products.AddRange(_mongoDbRepository.Find(filter).ToList().OrderByDescending(t => t.CreatedDate).Take(4));
+                products.AddRange(_mongoDbRepository.Find(filter).ToList().OrderByDescending(t => t.CreatedDate).Take(8));
             }else if(widget == "topSales")
             {
                 //wait for further update
