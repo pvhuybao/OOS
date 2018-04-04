c665eb127df80d1edc5b209428d5fc8b21b37520
LAPTOP-TM9CEKV9\Phuc <trieu.hphuc96@yahoo.com.vn>
2018-04-04 15:25:08 +0700

    up code - change api get Users list to get Paged Users list

---
+++ OOS/OOS.Presentation.ApplicationLogic/Users/Messages/GetUserRequest.cs
@@ -0,0 +1,16 @@
+﻿using OOS.Infrastructure.Queries;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOS.Presentation.ApplicationLogic.Users.Messages
+{
+    public class GetUserRequest : ListQueryBase
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
